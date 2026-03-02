d978a17dd65a60fa1c4d84c15d9193ab7a6892ac
heshanpasindusankalpa <pasindusankalpa2021@gmail.com>
2026-03-02 10:53:16 +0530

    UPDATE

--- stm32h743vit6_drivers/drivers/Src/stm32h743vit6_gpio_driver.c
+++ stm32h743vit6_drivers/drivers/Src/stm32h743vit6_gpio_driver.c
@@ -6,3 +6,46 @@
  */
 
 #include "stm32h743vit6_gpio_driver.h"
+
+//Peripheral clock setup
+void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx,uint8_t EnorDi)
+{
+
+}
+
+void GPIO_Init(GPIO_Handle_t *pGPIOHandle)
+{
+
+}
+void GPIO_DeInit(GPIO_RegDef_t *pGPIOx)
+{
+
+}
+uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t *pGPIOx,uint8_t PinNumber)
+{
+
+}
+uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *pGPIOx)
+{
+
+}
+void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGPIOx,uint8_t PinNumber,uint8_t Value)
+{
+
+}
+void GPIO_WriteToOutputPort(GPIO_RegDef_t *pGPIOx,uint8_t Value){
+
+}
+void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx,uint8_t PinNumber)
+{
+
+}
+
+void GPIO_IRQConfig(uint8_t TRQNumber,uint8_t IRQPriority,uint8_t EnorDi)
+{
+
+}
+void GPIO_IRQHandling(uint8_t PinNumber)
+{
+
+}
